d980137e2fbe748fbfe387610df09f772a4ff934
luizzitol <luizzitol@gmail.com>
2022-02-04 19:54:12 +0000

    created service class and service interface

---
+++ src/main/java/com/qa/clientsspringboot/service/ClientService.java
@@ -0,0 +1,4 @@
+package com.qa.clientsspringboot.service;
+
+public class ClientService {
+}
---
+++ src/main/java/com/qa/clientsspringboot/service/ClientServiceInterface.java
@@ -0,0 +1,4 @@
+package com.qa.clientsspringboot.service;
+
+public interface ClientServiceInterface {
+}
