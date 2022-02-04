180860311c8b32462158b7766182ecc1ea10053a
luizzitol <luizzitol@gmail.com>
2022-02-04 19:47:40 +0000

    extending JPA repository

--- src/main/java/com/qa/clientsspringboot/repo/ClientRepository.java
+++ src/main/java/com/qa/clientsspringboot/repo/ClientRepository.java
@@ -1,4 +1,9 @@
 package com.qa.clientsspringboot.repo;
 
-public class ClientRepository {
+import com.qa.clientsspringboot.domain.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+
 }
