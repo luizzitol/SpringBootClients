860702d7ce2b9c66c4059f029af68ee58866b6b2
luizzitol <luizzitol@gmail.com>
2022-02-04 21:03:34 +0000

    service interface wired

--- src/main/java/com/qa/clientsspringboot/controller/ClientController.java
+++ src/main/java/com/qa/clientsspringboot/controller/ClientController.java
@@ -1,4 +1,14 @@
 package com.qa.clientsspringboot.controller;
 
+import com.qa.clientsspringboot.service.ClientServiceInterface;
+
 public class ClientController {
+    private ClientServiceInterface clientService;
+
+
+    public ClientController(ClientServiceInterface clientService) {
+        this.clientService = clientService;
+    }
+
+
 }
