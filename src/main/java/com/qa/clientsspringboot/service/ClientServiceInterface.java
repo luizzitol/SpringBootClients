1a731456cbc597e8c50c82128961cee2cf80b864
luizzitol <luizzitol@gmail.com>
2022-02-04 20:01:20 +0000

    Service interface methods added

--- src/main/java/com/qa/clientsspringboot/service/ClientServiceInterface.java
+++ src/main/java/com/qa/clientsspringboot/service/ClientServiceInterface.java
@@ -1,4 +1,21 @@
 package com.qa.clientsspringboot.service;
 
+import com.qa.clientsspringboot.domain.Client;
+
+import java.util.List;
+
 public interface ClientServiceInterface {
+    //Create
+    Client createClient(Client client);
+
+    //Read
+    Client getClientById(Long id);
+    List<Client> getAllClients();
+    List<Client> getClientsByName();
+
+    //Update
+    Client updateClient(Long id, Client client);
+
+    //Delete
+    Client deleteClient(Long id);
 }
