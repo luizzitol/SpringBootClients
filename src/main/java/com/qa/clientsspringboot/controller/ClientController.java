e841f01787047196a18999bf2752242714dc04bd
luizzitol <luizzitol@gmail.com>
2022-02-04 21:11:43 +0000

    client controller CRUD implemented

--- src/main/java/com/qa/clientsspringboot/controller/ClientController.java
+++ src/main/java/com/qa/clientsspringboot/controller/ClientController.java
@@ -1,9 +1,10 @@
 package com.qa.clientsspringboot.controller;
 
+import com.qa.clientsspringboot.domain.Client;
 import com.qa.clientsspringboot.service.ClientServiceInterface;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/client")
@@ -16,7 +17,29 @@ public class ClientController {
         this.clientService = clientService;
     }
 
+    @PostMapping
+    public Client createClient(@RequestBody Client client){
+        return this.clientService.createClient(client);
 
+    }
 
+    @GetMapping
+    public List<Client> getAllClients(){
+        return this.clientService.getAllClients();
+    }
 
+    @GetMapping("/{id}")
+    public Client getClientById(@PathVariable Long id){
+        return this.clientService.getClientById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Client updateClient(@PathVariable Long id, @RequestBody Client client){
+        return this.clientService.updateClient(id, client);
+    }
+
+    @DeleteMapping("/{id}")
+    public Client deleteClient(@PathVariable Long id){
+        return this.clientService.deleteClient(id);
+    }
 }
