453ab871b5e02ecdf588f5de741a83dd8f0fa753
luizzitol <luizzitol@gmail.com>
2022-02-04 17:33:58 +0000

    added annotations in client class

--- src/main/java/com/qa/clientsspringboot/domain/Client.java
+++ src/main/java/com/qa/clientsspringboot/domain/Client.java
@@ -1,14 +1,28 @@
 package com.qa.clientsspringboot.domain;
 
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "client")
 public class Client {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Column(name = "dob")
     private LocalDate dob;
+
+    @Transient
     private int age;
 
 }
