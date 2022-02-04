322f6cbff204da0b29518f61cfc2f33632ba4a53
luizzitol <luizzitol@gmail.com>
2022-02-04 17:43:01 +0000

    added getters and setters and get Age logic

--- src/main/java/com/qa/clientsspringboot/domain/Client.java
+++ src/main/java/com/qa/clientsspringboot/domain/Client.java
@@ -3,6 +3,7 @@ package com.qa.clientsspringboot.domain;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table(name = "client")
@@ -24,5 +25,67 @@ public class Client {
 
     @Transient
     private int age;
+
+    public Client() {
+    }
+
+    public Client(String name, String email, LocalDate dob) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    public Client(Long id, String name, String email, LocalDate dob) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    public int getAge() {
+        return Period.between(this.dob, LocalDate.now()).getYears();
+    }
+
+    public Client setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Client setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Client setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Client setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public Client setDob(LocalDate dob) {
+        this.dob = dob;
+        return this;
+    }
+
 
 }
