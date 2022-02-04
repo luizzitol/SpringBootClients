c8e304c0fa1b368972ef532b3561f7a4e123dc7e
luizzitol <luizzitol@gmail.com>
2022-02-04 21:05:47 +0000

    added anotation in repository, service and controller classes

--- src/main/java/com/qa/clientsspringboot/controller/ClientController.java
+++ src/main/java/com/qa/clientsspringboot/controller/ClientController.java
@@ -1,7 +1,13 @@
 package com.qa.clientsspringboot.controller;
 
 import com.qa.clientsspringboot.service.ClientServiceInterface;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1/client")
+@CrossOrigin
 public class ClientController {
     private ClientServiceInterface clientService;
 
@@ -9,6 +15,8 @@ public class ClientController {
     public ClientController(ClientServiceInterface clientService) {
         this.clientService = clientService;
     }
+
+
 
 
 }
