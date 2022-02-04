47d66d54807ea3d7c85420136a2b394ba87fcf65
luizzitol <luizzitol@gmail.com>
2022-02-04 17:18:59 +0000

    SpringBoot project initialised and ERD added to Readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SpringBootClients
+
+In this project I will make a Full REST API using Spring Boot.
+
+The ERD that I am following is:
+
+![ERD](clientsERD.png)
---
+++ src/main/java/com/qa/clientsspringboot/ClientsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.qa.clientsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientsSpringBootApplication.class, args);
+    }
+
+}
