75fddb130e39e9f0d0b4b9ed639d54b61bc1bcdb
luizzitol <luizzitol@gmail.com>
2022-02-04 20:02:04 +0000

    client service interface implemented in client Service

--- src/main/java/com/qa/clientsspringboot/service/ClientService.java
+++ src/main/java/com/qa/clientsspringboot/service/ClientService.java
@@ -1,4 +1,37 @@
 package com.qa.clientsspringboot.service;
 
-public class ClientService {
+import com.qa.clientsspringboot.domain.Client;
+
+import java.util.List;
+
+public class ClientService implements ClientServiceInterface{
+    @Override
+    public Client createClient(Client client) {
+        return null;
+    }
+
+    @Override
+    public Client getClientById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Client> getAllClients() {
+        return null;
+    }
+
+    @Override
+    public List<Client> getClientsByName() {
+        return null;
+    }
+
+    @Override
+    public Client updateClient(Long id, Client client) {
+        return null;
+    }
+
+    @Override
+    public Client deleteClient(Long id) {
+        return null;
+    }
 }
