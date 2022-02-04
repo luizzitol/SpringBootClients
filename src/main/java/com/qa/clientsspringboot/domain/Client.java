a1d5f7bfe0a706ddb92d233aa8732ab86ec27fe3
luizzitol <luizzitol@gmail.com>
2022-02-04 17:30:46 +0000

    added main fields in clients class

--- src/main/java/com/qa/clientsspringboot/domain/Client.java
+++ src/main/java/com/qa/clientsspringboot/domain/Client.java
@@ -1,4 +1,14 @@
 package com.qa.clientsspringboot.domain;
 
+
+import java.time.LocalDate;
+
 public class Client {
+
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private int age;
+
 }
